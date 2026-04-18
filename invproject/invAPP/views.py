5be809a267ab8d91252bbc9ffc8a2b269e0b189e
isaacandresbaleero-hub <isaacandresbaleero@gmail.com>
2026-04-18 15:41:08 +0300

    created Templates in app folder

--- invproject/invAPP/views.py
+++ invproject/invAPP/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from .forms import ProductForm
+from .models import product
+
 # Create your views here.
+#CRUD = Create, Read, Update, Delete
+
+#Create View 
+
+#Home Iview
+def home_view(request)
+    
+#Read View 
+#Update View 
+#Delete View 
