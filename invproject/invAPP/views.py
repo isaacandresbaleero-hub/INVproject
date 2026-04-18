cc16d2e67758c4bdf1d1e3542e421480c1239400
isaacandresbaleero-hub <isaacandresbaleero@gmail.com>
2026-04-18 16:36:44 +0300

    updates views fieled for my app

--- invproject/invAPP/views.py
+++ invproject/invAPP/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, ridirect
 
 from .forms import ProductForm
 from .models import product
@@ -7,10 +7,41 @@ from .models import product
 #CRUD = Create, Read, Update, Delete
 
 #Create View 
+def home_view(request):
+    return render(request, 'invApp/home.html')
 
 #Home Iview
-def home_view(request)
+def product_create_view(request):
+    form = ProductForm()
+    if request.method = 'POST'
+        form = ProductForm(request.form)
+        if form .is_valid():
+            form.save()
+            return ridirect ('product_list')
+    return render(request, 'invApp/product_form.html' {'form':form})
     
 #Read View 
+def product_list_view(request):
+    product = products.objects.all()
+    return render(request, 'InvApp/product_list.html', {'products':product})
+
 #Update View 
+def product_update_view(request):
+    product.objects.get(product_id=product_id )
+    form = ProductForm(instance = product)
+    if request.method == "POST":
+        ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save():
+            return redirect('product_list')
+    return render(request, 'invApp/product_form.html' {'form':form})
+
 #Delete View 
+def product_delete_view(request, product_id):
+    product.objects.get(product_id=product_id )
+    form = ProductForm(instance = product)
+    if request.method == "POST":
+        Product.delete()
+        return redirect('product_list')
+    
+    return render (request, 'invApp/product_comfirm_delete.html', {'products':product})
